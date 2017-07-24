d5e0fea37261c0fbd084b98af0998ea4012cb8ac
Adrian-Yao <adrian.c.yao@gmail.com>
2017-07-24 09:37:25 -0400

    Make School Notes Back-up

---
+++ MakeSchoolNotes-Swift-V2-Starter-swift3-coredata/MakeSchoolNotes/Models/Note Data Model.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  MakeSchoolNotes
+//
+//  Created by adyao20 on 7/19/17.
+//  Copyright © 2017 MakeSchool. All rights reserved.
+//
+
+import Foundation
+
+class Note {
+    var title = ""
+    var content = ""
+    var modificationTime = Date()
+}
